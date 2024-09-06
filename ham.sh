d9783de9e95ffac1a6d11f4fa24915354f538b9d
Anatol Kaczmarek <92335424+Tole-k@users.noreply.github.com>
2024-09-06 18:05:02 +0200

    fix bug

--- ham.sh
+++ ham.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-# Path to the counter file
-COUNTER_FILE=".prompt_counter"
-HAM_FILE="All_Lyrics_No_Speakers"
+# Determine the directory of the script
+SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
+
+# Path to the counter file and HAM file in the repository directory
+COUNTER_FILE="$SCRIPT_DIR/.prompt_counter"
+HAM_FILE="$SCRIPT_DIR/All_Lyrics_No_Speakers"
+
+# Check if HAM_FILE exists and is not empty
+if [ ! -f "$HAM_FILE" ] || [ ! -s "$HAM_FILE" ]; then
+    echo "Error: HAM_FILE does not exist or is empty."
+    return 1
+fi
+
 HAM_LENGTH=$(wc -l < "$HAM_FILE")
 
 # Function to update the prompt
@@ -30,4 +40,3 @@ export -f update_prompt
 
 # Set the PROMPT_COMMAND to call update_prompt before displaying each prompt
 PROMPT_COMMAND="update_prompt"
-
--- install.sh
+++ install.sh
@@ -2,10 +2,9 @@
 
 rcFile="$HOME/.bashrc"
 
-if grep -q "source $PWD/ham.sh" $rcFile; then
+if grep -q "source $PWD/ham.sh" "$rcFile"; then
     echo "ham.sh is already installed"
 else
     echo "Installing ham.sh"
-    echo "source $PWD/ham.sh" >> $rcFile
+    echo "source $PWD/ham.sh" >> "$rcFile"
 fi
-
